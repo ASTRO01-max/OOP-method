a4a84e19e5907ddd40a7820de83468d296d7f4e1
ASTRO01-max <mfulsefer@gmail.com>
2025-01-28 15:24:53 +0500

    message

--- static.py
+++ static.py
@@ -20,23 +20,11 @@ class Static:
         letters = [char for char in text if char.isalpha()]
         return ''.join(sorted(letters))
 
-    @static_remove_numbers_text
-
 static_class = Static()
 
-print(static_class.static_islower_text("hello")) 
-print(static_class.static_isupper_text("HELLO"))
-print(static_class.static_remove_numbers_text("123hello456world789"))
-print(static_class.static_remove_numbers_sorted_text("321cba456"))
-print(static_class.static_remove_numbers_sorted_text("z5a4x3c2v1"))
+print(static_class.static_islower_text("hello"))  
+print(static_class.static_isupper_text("HELLO"))   
+print(static_class.static_remove_numbers_text("123hello456world789")) 
+print(static_class.static_remove_numbers_sorted_text("321cba456")) 
+print(static_class.static_remove_numbers_sorted_text("z5a4x3c2v1"))  
 
-
-def max_absolute_difference(nums):
-    n = len(nums)
-    max_diff = 0
-
-    for i in range(n):
-        diff = abs(nums[i] - nums[(i + 1) % n])
-        max_diff = max(max_diff, diff)
-
-    return max_diff
